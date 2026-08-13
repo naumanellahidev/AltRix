607c8b8726c1a539aba67ef371ab52485f9d2582
farhathashmireflections-sys <farhathashmireflections@gmail.com>
2026-08-13 23:57:19 +0500

    fix: support arguments in backend/entrypoint.sh to enable Celery containers

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+if [ $# -gt 0 ]; then
+  exec "$@"
+else
+  exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+fi
